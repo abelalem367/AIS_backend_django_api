eae539efe91d6ebb35bf1b9de0df13d11b3b5243
Abela <abelzerabruk@gmail.com>
2023-04-10 13:04:05 +0300

    small commit, simply configured urls.py

--- ais/urls.py
+++ ais/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('api/ais/viewdata', views.getData),
-    path('api/ais/adminlogin',views.adminLogin),
-    path('api/ais/expertlogin/',views.expertLogin),
-    path('api/ais/garagelogin/',views.garageLogin),
-    path('api/ais/proposerlogin/',views.proposerLogin),
-    path('api/ais/creategarageaccount/',views.createGarageAccount),
-    path('api/ais/createexpertaccount/',views.createExpertAccount),
-    path('api/ais/createproposeraccount/',views.createProposerAccount),
+    path('viewdata', views.getData),
+    path('adminlogin',views.adminLogin),
+    path('expertlogin/',views.expertLogin),
+    path('garagelogin/',views.garageLogin),
+    path('proposerlogin/',views.proposerLogin),
+    path('creategarageaccount/',views.createGarageAccount),
+    path('createexpertaccount/',views.createExpertAccount),
+    path('createproposeraccount/',views.createProposerAccount),
 ]
